6c09dbdf69123733f840319740b4cb851774c75a
Pruds <prudenciopvp@gmail.com>
2023-10-23 22:08:19 -0300

    first commit

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,3 @@
-// let name = prompt('Me diga seu nome:');
-// alert('Seja bem vindo ' + name + '. Espero que goste do site!!');
-// let curso = prompt('Qual seu curso?');
-
-// if (curso == "ds") {
-//   alert('Boaa!!')
-// }
-
-// else if (curso == 'DS') {
-//   alert('Boaa!!')
-// }
-
-// else if (curso == 'Desenvolvimento de Sistemas') {
-//   alert('Boaa!!')
-// }
-
-// else if (curso == 'desenvolvimento de sistemas') {
-//   alert('Boaa!!')
-// }
-
-// else {
-//   alert('Vai estudar algo que preste')
-// }
-
 function linkedin() {
     window.open("https://www.google.com/search?q=linkedin&sca_esv=572065970&tbm=isch&source=lnms&sa=X&ved=2ahUKEwip5_qvneqBAxV3ILkGHWNDCvoQ_AUoAnoECAEQBA&biw=1600&bih=783&dpr=1") 
 }
